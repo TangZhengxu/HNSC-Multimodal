fb461451c88ba977675c45685cd411bcf184facb
TangZhengxu <123335659+TangZhengxu@users.noreply.github.com>
2024-06-21 17:09:50 -0400

    Update t1_to_nii.py

--- script/preprocessing/t1_to_nii.py
+++ script/preprocessing/t1_to_nii.py
@@ -4,7 +4,6 @@ import pydicom
 import nibabel as nib
 
 def load_dicom_series(dicom_dir):
-    # Get a list of all DICOM files in the directory
     dicom_files = []
     for root, dirs, files in os.walk(dicom_dir):
         for file in files:
@@ -25,16 +24,33 @@ def load_dicom_series(dicom_dir):
         ds = pydicom.dcmread(file)
         image[:, :, i] = ds.pixel_array
 
-    return image, ref_ds
+    pixel_spacing = ref_ds.PixelSpacing
+    slice_thickness = ref_ds.SliceThickness
+
+    slice_positions = [pydicom.dcmread(f).ImagePositionPatient[2] for f in dicom_files]
+    slice_positions = np.array(slice_positions)
+    slice_positions.sort()
+    slice_thickness = np.mean(np.diff(slice_positions))
+
+    orientation = ref_ds.ImageOrientationPatient
+    position = ref_ds.ImagePositionPatient
+
+    affine = np.eye(4)
+    affine[0, :3] = np.array(orientation[:3]) * pixel_spacing[0]
+    affine[1, :3] = np.array(orientation[3:]) * pixel_spacing[1]
+    affine[2, 2] = slice_thickness
+    affine[:3, 3] = position
+
+    return image, affine
 
 def dicom_to_nifti(dicom_dir, output_file):
-    image, ref_ds = load_dicom_series(dicom_dir)
+    image, affine = load_dicom_series(dicom_dir)
 
-    if image is None or ref_ds is None:
+    if image is None or affine is None:
         print(f"No DICOM files found in {dicom_dir}")
         return
 
-    nifti_img = nib.Nifti1Image(image, np.eye(4))
+    nifti_img = nib.Nifti1Image(image, affine)
 
     nib.save(nifti_img, output_file)
 
@@ -47,13 +63,14 @@ def process_all_patients(data_root, target_folder):
         if not os.path.isdir(patient_path):
             continue
 
-        pre_rt_folder = os.path.join(patient_path, '2wkRT')
+        pre_rt_folder = os.path.join(patient_path, 'preRT')
 
         patient_id = patient_folder[:4]
-        output_nifti_file = os.path.join(target_folder, f'{patient_id}_post_mri_t1.nii')
+        output_nifti_file = os.path.join(target_folder, f'{patient_id}_pre_mri_t1.nii')
 
         dicom_to_nifti(pre_rt_folder, output_nifti_file)
 
+
 data_root = '/RadOnc-MRI1/Student_Folder/tangzx/Data/HN2013_062_T1_Vp_ADC_FDG/'
-target_folder = '/RadOnc-MRI1/Student_Folder/tangzx/Data/private_data_head_neck/mri_post_t1'
+target_folder = '/RadOnc-MRI1/Student_Folder/tangzx/Data/private_data_head_neck/mri_pre_t1_new_new'
 process_all_patients(data_root, target_folder)
